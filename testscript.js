3f54932466bab2f81ae354faa1e929673ead0753
ansh7mehta <ansh7mehta@gmail.com>
2022-11-13 00:03:35 +0530

    Update testscript.js

--- testscript.js
+++ testscript.js
@@ -1,5 +1,4 @@
-<script>
-<![CDATA[
+![CDATA[
 alert("Discount Applied! Prices will be updated at checkout.");
 let url_string = "/discount/";
 var urlParams = new URLSearchParams(window.location.search);
@@ -8,5 +7,4 @@ let aff = params.id;
 let discountURL = url_string + aff;
 let postURL = "https://postandearn.com/version-test/api/1.1/wf/visit_stats?id=" + aff;
 fetch(postURL, {method : "POST", mode: 'no-cors'});
-location.replace(discountURL);]]>
-</script>
+location.replace(discountURL);]]
