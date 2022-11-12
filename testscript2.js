c808c686c37028a5b4372529494277e5f7759b43
ansh7mehta <ansh7mehta@gmail.com>
2022-11-13 00:09:37 +0530

    Create testscript2.js

---
+++ testscript2.js
@@ -0,0 +1,9 @@
+alert("Discount Applied! Prices will be updated at checkout.");
+let url_string = "/discount/";
+var urlParams = new URLSearchParams(window.location.search);
+const params = new Proxy(new URLSearchParams(window.location.search), {get: (searchParams, prop) => searchParams.get(prop),});
+let aff = params.id;
+let discountURL = url_string + aff;
+let postURL = "https://postandearn.com/version-test/api/1.1/wf/visit_stats?id=" + aff;
+fetch(postURL, {method : "POST", mode: 'no-cors'});
+location.replace(discountURL);
